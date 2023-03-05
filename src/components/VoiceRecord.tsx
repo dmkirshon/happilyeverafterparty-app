88c34531007ca90fa6248718c38476371d603db6
dmkirshon <kirshon.david@gmail.com>
2023-03-05 08:19:18 +0200

    Feat: Make first connection to Firebase storage

--- src/components/VoiceRecord.tsx
+++ src/components/VoiceRecord.tsx
@@ -1,12 +1,23 @@
 import { AudioRecorder } from "react-audio-voice-recorder";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+import app from "../firebaseConfig";
+import { useState } from "react";
 
 export const VoiceRecord = () => {
+  const [name, setName] = useState("name");
+  const storage = getStorage(app);
+
   const addAudioElement = (blob: Blob) => {
-    const url = URL.createObjectURL(blob);
-    const audio = document.createElement("audio");
-    audio.src = url;
-    audio.controls = true;
-    document.body.appendChild(audio);
+    const audioRef = ref(storage, `audio/${name}-${new Date().valueOf()}`);
+    uploadBytes(audioRef, blob).then((snapshot) => {
+      console.log("Uploaded a blob or file!");
+    });
+
+    // const url = URL.createObjectURL(blob);
+    // const audio = document.createElement("audio");
+    // audio.src = url;
+    // audio.controls = true;
+    // document.body.appendChild(audio);
   };
 
   return (
