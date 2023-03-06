554b7112e83d5fd3636e1b87c578fb8bbfb00ed6
dmkirshon <kirshon.david@gmail.com>
2023-03-06 09:41:59 +0200

    feat: Add initial styles for home page

--- src/components/UserIdentifier.tsx
+++ src/components/UserIdentifier.tsx
@@ -24,7 +24,7 @@ const UserIdentifier = ({
         }}
       >
         <label>
-          Name:{" "}
+          Name
           <input
             type="text"
             value={name}
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,24 @@
 import { Link } from "react-router-dom";
+import background from "../assets/3.png";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome</h1>
+    <div
+      style={{
+        minWidth: "100vw",
+        minHeight: "100vh",
+        background: `#000 url(${background}) no-repeat center`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+        backgroundAttachment: "fixed",
+        backgroundSize: "contain",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1 style={{ fontSize: "72px" }}>Welcome</h1>
       <Link to="voice-box">Leave a Voice Message</Link>
     </div>
   );
