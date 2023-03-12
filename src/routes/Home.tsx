311bd4b9ba9af3f3f1c5c92ca777134da9ff2f5f
dmkirshon <kirshon.david@gmail.com>
2023-03-12 19:13:50 +0200

    feat: Update styling for more readability

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,15 +3,22 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="home_container">
-      <h1 className="home_title" style={{ fontSize: "72px" }}>
-        Welcome
-      </h1>
-      <Link className="home_link link_navigation" to="voice-box">
-        Leave a Voice Message
-      </Link>
-      <Link to="https://weduploader.com/upload/O0H3empAUgXVCoxQ">
-        Upload Photos
-      </Link>
+      <h1 className="home_title">Welcome</h1>
+      <div className="home_links">
+        <Link className="link_nav" to="voice-box">
+          ~ Voice Note ~
+          <p className="link_subtitle">Record a voice note for the couple</p>
+        </Link>
+        <Link
+          className="link_nav"
+          to="https://weduploader.com/upload/O0H3empAUgXVCoxQ"
+        >
+          ~ Share Moments ~
+          <p className="link_subtitle">
+            Upload photos and videos during the night
+          </p>
+        </Link>
+      </div>
       <footer className="home_footer">
         <p>Designed by Devon</p>
         <p>Created by David</p>
